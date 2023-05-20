fe1e1676899d10cd6dae8b0e40b1ce6da9bb115f
Thiago Pessoa <thiagokxl5@gmail.cpm>
2023-05-20 12:14:58 -0300

    MEtodos da controller GetAll, Get e Insert

--- MeuTodo/Controller/TodoController.cs
+++ MeuTodo/Controller/TodoController.cs
@@ -1,6 +1,12 @@
-﻿using MeuTodo.Models;
+﻿using MeuTodo.Data;
+using MeuTodo.Models;
+using MeuTodo.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace MeuTodo.Controller
 {
@@ -10,9 +16,51 @@ namespace MeuTodo.Controller
     {
         [HttpGet]
         [Route(template:"todos")]
-        public List<Todo> GetAll()
+        public async Task<IActionResult> GetAll([FromServices] AppDbContext context)
         {
-            return new List<Todo>();
+            var todos = await context.Todos
+                .AsNoTracking()
+                .ToListAsync();
+            return Ok(todos);
+        }
+
+        [HttpGet]
+        [Route(template: "todos/{id}")]
+        public async Task<IActionResult> Get([FromServices] AppDbContext context, [FromRoute] int id)
+        {
+            var todo = await context.Todos
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == id);
+            return todo == null 
+                ? NotFound()
+                : Ok(todo);
+        }
+
+        [HttpPost]
+        [Route(template: "todo/insert")]
+        public async Task<IActionResult> Insert([FromServices] AppDbContext context, [FromBody] CreateTodoViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            var todo = new Todo
+            {
+                Date = DateTime.Now,
+                Done = false,
+                Title = model.Title
+            };
+            
+            try
+            {
+                await context.Todos.AddAsync(todo);
+                await context.SaveChangesAsync();
+                return Created(uri: $"v1/todos/{todo.Id}", todo);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
     }
 }
--- MeuTodo/Startup.cs
+++ MeuTodo/Startup.cs
@@ -1,3 +1,4 @@
+using MeuTodo.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -18,6 +19,7 @@ namespace MeuTodo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<AppDbContext>();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
